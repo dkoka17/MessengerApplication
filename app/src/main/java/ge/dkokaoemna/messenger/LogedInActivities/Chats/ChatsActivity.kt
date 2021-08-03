b31cd6eb2cdee38eaa0ce3fe55c76a74111823c8
dkoka17 <dkoka17@freeuni.edu.ge>
2021-08-03 23:47:35 +0400

    upd

--- app/src/main/java/ge/dkokaoemna/messenger/LogedInActivities/Chats/ChatsActivity.kt
+++ app/src/main/java/ge/dkokaoemna/messenger/LogedInActivities/Chats/ChatsActivity.kt
@@ -1,29 +1,47 @@
 package ge.dkokaoemna.messenger.LogedInActivities.Chats
 
+import android.content.Intent
 import android.os.Bundle
-import android.os.PersistableBundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 import ge.dkokaoemna.messenger.Firebase.models.Chat
+import ge.dkokaoemna.messenger.Firebase.models.User
+import ge.dkokaoemna.messenger.LogedInActivities.chatWithFriend.chatWithFriendActivity
 import ge.dkokaoemna.messenger.R
+import java.io.Serializable
+import java.util.*
 import java.util.Collections.emptyList
 
-class ChatsActivity : AppCompatActivity() {
+
+class ChatsActivity : AppCompatActivity(), IChatsObjView {
 
     private lateinit var recView: RecyclerView
-    private  var adapter: ChatsListAdapter = ChatsListAdapter(emptyList())
+    private  var adapter: ChatsListAdapter = ChatsListAdapter(emptyList(), ::onItemClicked)
+    private lateinit var presenter: ChatPresnter
+
+    private lateinit var database: FirebaseDatabase
+    private lateinit var auth: FirebaseAuth;
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.chats_layout)
 
+        database = Firebase.database
+        auth = Firebase.auth
 
         val chatItems : RecyclerView = findViewById(R.id.chatsRecycler)
         chatItems.layoutManager = StaggeredGridLayoutManager(1, StaggeredGridLayoutManager.VERTICAL)
-
-        adapter = ChatsListAdapter(emptyList())
 
         recView = findViewById(R.id.chatsRecycler)
         recView.adapter = adapter
@@ -31,6 +49,38 @@ class ChatsActivity : AppCompatActivity() {
         val other: List<Chat> = emptyList()
         adapter.list = other
         adapter.notifyDataSetChanged()
+
+        database.getReference("Users").child(auth?.uid!!).addValueEventListener(object :
+                ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+
+                var user: User = dataSnapshot.getValue(User::class.java) as User
+
+                //TODO user გამოვიყენოთ შესაბამის ველების შესაასვებად, რაიმე თ შეიცვალა, მესისჯიჯ მოვიდა და ა.შ. ავტომატურად განახლება
+
+                adapter.list = user.chats
+                adapter.notifyDataSetChanged()
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Toast.makeText(this@ChatsActivity, "error", Toast.LENGTH_SHORT).show()
+            }
+        })
+
+        presenter = ChatPresnter(this)
+    }
+
+    override fun showChatObjList(ChatObjs: List<Chat>) {
+        //TODO("Not yet implemented")
+    }
+
+    fun onItemClicked(chatObj: Chat){
+
+        val intent = Intent(applicationContext, chatWithFriendActivity::class.java)
+        intent.putExtra("chat", chatObj as Serializable)
+        startActivity(intent)
+
+
     }
 }
 
