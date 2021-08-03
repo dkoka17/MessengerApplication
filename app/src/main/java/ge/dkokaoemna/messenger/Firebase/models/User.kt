6730fc0b6e063bbd2becd84edcec3d606f259d84
dkoka17 <dkoka17@freeuni.edu.ge>
2021-08-03 12:59:35 +0400

    LogIN back functions

--- app/src/main/java/ge/dkokaoemna/messenger/Firebase/models/User.kt
+++ app/src/main/java/ge/dkokaoemna/messenger/Firebase/models/User.kt
@@ -1,6 +1,7 @@
 package ge.dkokaoemna.messenger.Firebase.models
 
 import com.google.firebase.database.IgnoreExtraProperties
+import java.util.*
 
 @IgnoreExtraProperties
-data class User(val firstName: String? = null, val lastName: String? = null, val isActive: Boolean? = null)
+data class User(val name: String? = null, val nickname: String? = null, val job: String? = null, val chats: ArrayList<Chat>)
--- app/src/main/java/ge/dkokaoemna/messenger/authentification/LogInActivity.kt
+++ app/src/main/java/ge/dkokaoemna/messenger/authentification/LogInActivity.kt
@@ -1,12 +1,19 @@
-package ge.dkokaoemna.messenger.authentification
+  package ge.dkokaoemna.messenger.authentification
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import ge.dkokaoemna.messenger.Firebase.models.Chat
+import ge.dkokaoemna.messenger.Firebase.models.Sms
+import ge.dkokaoemna.messenger.Firebase.models.User
+import ge.dkokaoemna.messenger.LogedInActivities.Chats.ChatsActivity
 import ge.dkokaoemna.messenger.R
+import java.util.*
 
 class LogInActivity  : AppCompatActivity()  {
 
@@ -15,27 +22,81 @@ class LogInActivity  : AppCompatActivity()  {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.sign_in_layout)
-
         auth = Firebase.auth
     }
 
-    fun createUser(){
-        var email= "dkok@gmail.com"
+    fun logIN(){
+        var email= "dkasf12423ok@gmail.com"
         var password = "123Qwesa"
 
-        auth.createUserWithEmailAndPassword(email, password)
+        auth.signInWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this) { task ->
                     if (task.isSuccessful) {
                         // Sign in success, update UI with the signed-in user's information
-                        Toast.makeText(this@LogInActivity, "createUserWithEmail:success", Toast.LENGTH_SHORT).show()
-                        val user = auth.currentUser
+                        Toast.makeText(this@LogInActivity, "Loge In:success", Toast.LENGTH_SHORT).show()
+
+                        moveToChat()
                     } else {
-                        // If sign in fails, display a message to the user.
-                        Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.makeText(baseContext, "Loge In failed.",
                                 Toast.LENGTH_SHORT).show()
 
                     }
                 }
 
+    }
+
+    //TODO: for developing, delete later
+    fun logOut(){
+        auth.signOut()
+    }
+
+    fun createUser(){
+        var email= "dkasf12423ok@gmail.com"
+        // TODO: შევაყვანინოთ მხოლოდ nickname და "@gmail.com" ჩვენით დავამატოთ, რადგან ბაზაში რეგისტრაცია მეილით შეიძლება მხოლოდ. nickname-email ეგრე გადმოეცი ფუნქციას
+        var nickname = "dato"
+        var password = "123Qwesa"
+        var job = "deve"
+
+        auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+
+                        Toast.makeText(this@LogInActivity, "createUser:success", Toast.LENGTH_SHORT).show()
+
+                        val database = Firebase.database
+                        val ref = database.getReference("Users")
+
+                        // TODO delete next 3 line, leave for time
+                        var sm = Sms("asf3","saf3")
+                        var arrList1 = ArrayList<Sms>()
+                        var ch = Chat("123", arrList1)
+                        // END
+
+                        var arrList2 = ArrayList<Chat>()
+                        var user = User(nickname,nickname,job,arrList2)
+
+                        auth = Firebase.auth
+                        val currentUser = auth.currentUser
+                        var key = currentUser?.uid!!
+                        ref.child(key).setValue(user)
+
+
+                        moveToChat()
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Toast.makeText(baseContext, "createUser failed.",
+                                Toast.LENGTH_SHORT).show()
+
+                    }
+                }
+
+
+    }
+
+
+    fun moveToChat(){
+        val intent = Intent(this, ChatsActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
